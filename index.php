3ee72d42cea911cfc4e8bf155b58a50b572813de
Ganesh <rahulmirji444@gmail.com>
2024-08-08 21:02:11 +0530

    updated index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 
         .container {
             width: 80%;
-            margin: 0 auto;
+            margin:  auto;
         }
 
         .header-content {
@@ -99,9 +99,9 @@
 <?php include 'nav.php'; ?>
 
 <header>
-    <div class="container">
+    <div class="container"> 
         <div class="header-content">
-            <h1 align="left">Websites Done Right</h1>
+            <h1 align="center ">Websites Done Right</h1>
             <p>I am a web developer from Bengaluru, India, and I currently live in Bengaluru. I enjoy building everything from small business sites to rich interactive web apps. If you are a business seeking a web presence or an employer looking to hire, you can get in touch with me <a href="contact.php">here</a>.</p>
             <div class="cta-buttons">
                 <a href="i_need_web.php" class="btn">I need a website <span>&#10145;</span></a>
